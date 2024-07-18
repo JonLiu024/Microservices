02af27cc06c16535d7e373b19376cee46a630cc0
Liu, Tong <tliu024@student.ubc.ca>
2024-07-18 00:41:01 -0700

    updated pom files to replace the spring sleuth dependencies with micrometer dependency, added span tracing in place order methods

--- order-services/src/main/java/com/jonltech/order_service/service/OrderService.java
+++ order-services/src/main/java/com/jonltech/order_service/service/OrderService.java
@@ -4,10 +4,14 @@ import com.jonltech.order_service.dto.*;
 import com.jonltech.order_service.model.Order;
 import com.jonltech.order_service.model.OrderLineItems;
 import com.jonltech.order_service.repository.OrderRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -19,76 +23,88 @@ import static java.util.stream.Collectors.toList;
 @RequiredArgsConstructor
 @Builder
 @Service
+@Transactional
+@Slf4j
 public class OrderService {
 
     public final OrderRepository orderRepository;
     public final WebClient.Builder webClientBuilder;
+    public final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
+        Span newSpan = this.tracer.nextSpan().name("placeOrder");
+        try (Tracer.SpanInScope ws = this.tracer.withSpan(newSpan.start())) {
 
-        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemDtosList()
-                .stream().map(this::mapOrderLineItemDtoToPojo).collect(toList());
-        order.setOrderLineItemsList(orderLineItemsList);
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
 
-        Map<String, Integer> orderLineItemQuantities = new HashMap<>();
-        for (OrderLineItems orderLineItems: orderLineItemsList) {
-            orderLineItemQuantities.put(orderLineItems.getSkuCode(), orderLineItems.getQuantity());
-        }
+            List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemDtosList()
+                    .stream().map(this::mapOrderLineItemDtoToPojo).collect(toList());
+            order.setOrderLineItemsList(orderLineItemsList);
 
-        List<String> skuCodes = orderLineItemsList.stream().map(
-                OrderLineItems::getSkuCode).toList();
+            Map<String, Integer> orderLineItemQuantities = new HashMap<>();
+            for (OrderLineItems orderLineItems: orderLineItemsList) {
+                orderLineItemQuantities.put(orderLineItems.getSkuCode(), orderLineItems.getQuantity());
+            }
 
-
-        //make request to the inventory service to check if orderlineitems are in stock
-        //uriBuilder.queryparam creates the uri in the form localhost:8082/api/inventory?skuCode=..&skuCode2=...
-        //-> localhost:8082/api/inventory?skuCode={skuCode1}.. and localhost:8082/api/inventory?skuCode={skuCode}....
+            List<String> skuCodes = orderLineItemsList.stream().map(
+                    OrderLineItems::getSkuCode).toList();
 
 
-        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
-                .uri("http://localhost:8081/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()
-                )
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+            //make request to the inventory service to check if orderlineitems are in stock
+            //uriBuilder.queryparam creates the uri in the form localhost:8082/api/inventory?skuCode=..&skuCode2=...
+            //-> localhost:8082/api/inventory?skuCode={skuCode1}.. and localhost:8082/api/inventory?skuCode={skuCode}....
 
 
-        System.out.println("Inventory check responses: " + Arrays.toString(inventoryResponses));
+            InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                    .uri("http://localhost:8081/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()
+                    )
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        assert inventoryResponses != null;
-        boolean hasEnoughProductsInstock = Arrays.stream(inventoryResponses).allMatch(inventoryResponse ->
-                inventoryResponse.getStock() >=
-                        orderLineItemQuantities.get(inventoryResponse.getSkuCode()));
 
-        if (hasEnoughProductsInstock) {
-            orderRepository.save(order);
-            List<UpdateInventoryRequest> updateInventoryRequestList =
-                    orderLineItemsList.stream().map(orderLineItems -> new UpdateInventoryRequest(
-                    orderLineItems.getSkuCode(),
-                    orderLineItems.getQuantity())
-            ).toList();
+            System.out.println("Inventory check responses: " + Arrays.toString(inventoryResponses));
 
-            try {
-                webClientBuilder.build().post().uri("localhost:8081/api/inventory/update")
-                        .bodyValue(BodyInserters.fromValue(updateInventoryRequestList))
-                        .retrieve()
-                        .bodyToMono(void.class)
-                        .block();
-                System.out.println("Inventory updated");
-                return "placing order successfully";
+            assert inventoryResponses != null;
+            boolean hasEnoughProductsInstock = Arrays.stream(inventoryResponses).allMatch(inventoryResponse ->
+                    inventoryResponse.getStock() >=
+                            orderLineItemQuantities.get(inventoryResponse.getSkuCode()));
 
-            } catch (Exception e) {
-                System.out.println("Inventory update is not successful");
-                orderRepository.delete(order);
-                System.out.println("order is deleted from the repository");
-                return "place order failed";
+            if (hasEnoughProductsInstock) {
+                orderRepository.save(order);
+                List<UpdateInventoryRequest> updateInventoryRequestList =
+                        orderLineItemsList.stream().map(orderLineItems -> new UpdateInventoryRequest(
+                                orderLineItems.getSkuCode(),
+                                orderLineItems.getQuantity())
+                        ).toList();
+
+                try {
+                    webClientBuilder.build().post().uri("localhost:8081/api/inventory/update")
+                            .bodyValue(BodyInserters.fromValue(updateInventoryRequestList))
+                            .retrieve()
+                            .bodyToMono(void.class)
+                            .block();
+                    System.out.println("Inventory updated");
+                    return "placing order successfully";
+
+                } catch (Exception e) {
+                    System.out.println("Inventory update is not successful");
+                    orderRepository.delete(order);
+                    System.out.println("order is deleted from the repository");
+                    return "place order failed";
+                }
+
+
+            } else {
+                throw new IllegalArgumentException("The product ordered is not in stock");
             }
 
 
-        } else {
-            throw new IllegalArgumentException("The product ordered is not in stock");
+        } finally {
+            newSpan.end();
         }
+
 
 
     }
