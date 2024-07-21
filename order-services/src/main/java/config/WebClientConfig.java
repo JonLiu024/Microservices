35e61815ad1a9e98482e631b545cb9d7c05192f1
Liu, Tong <tliu024@student.ubc.ca>
2024-07-20 17:50:58 -0700

    modified the application name, spring routes properties in api-gateway

--- order-services/src/main/java/config/WebClientConfig.java
+++ order-services/src/main/java/config/WebClientConfig.java
@@ -3,9 +3,11 @@ package config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@EnableWebFlux
 public class WebClientConfig {
 
     @Bean
