0944d66e95106a2a9482e9c08cb65a9e2f93a445
Liu, Tong <tliu024@student.ubc.ca>
2024-07-28 18:32:03 -0700

    added service events in inventory service, product service, added kafka properties in inveotry service and product service modules, added src code for notification service

--- inventory-services/src/main/java/com/jonltech/inventory_service/service/InventoryService.java
+++ inventory-services/src/main/java/com/jonltech/inventory_service/service/InventoryService.java
@@ -2,10 +2,12 @@ package com.jonltech.inventory_service.service;
 
 import com.jonltech.inventory_service.dto.InventoryResponse;
 import com.jonltech.inventory_service.dto.UpdateInventoryRequest;
+import com.jonltech.inventory_service.event.InventoryUpdateEvent;
 import com.jonltech.inventory_service.model.Inventory;
 import com.jonltech.inventory_service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,7 @@ import java.util.List;
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
+    private final KafkaTemplate<String, InventoryUpdateEvent> kafkaTemplate;
 
     @Transactional(readOnly = true)
     public List<InventoryResponse> getStockInfo(List<String> skuCode) throws InterruptedException {
@@ -28,12 +31,14 @@ public class InventoryService {
 
 
     @Transactional
-    public void updateInventory(List<UpdateInventoryRequest> updateInventoryRequests) {
+    public void updateInventoryForOrderPlaced(List<UpdateInventoryRequest> updateInventoryRequests) {
 
         for (UpdateInventoryRequest updateInventoryRequest : updateInventoryRequests) {
             Inventory inventory = inventoryRepository.findBySkuCode(updateInventoryRequest.getSkuCode());
             inventory.setQuantity(inventory.getQuantity() - updateInventoryRequest.getQuantity());
             inventoryRepository.save(inventory);
+            kafkaTemplate.send("notificationTopic", new InventoryUpdateEvent(inventory.getSkuCode()));
+
         }
     }
 
---
+++ product-services/src/main/java/com/jonltech/product_services/event/ProductCreatedEvent.java
@@ -0,0 +1,12 @@
+package com.jonltech.product_services.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCreatedEvent {
+    String productId;
+}
